c8a7fc643e94473b9eb59a5d3ec91fdc7c321563
keyhoh <iheyhoh+github@gmail.com>
2021-05-01 14:20:56 +0900

    add: fields

--- src/main/java/com/example/demo/domain/model/Task.java
+++ src/main/java/com/example/demo/domain/model/Task.java
@@ -4,4 +4,12 @@ package com.example.demo.domain.model;
  * タスク
  */
 public class Task {
+    /**
+     * タイトル
+     */
+    String title;
+    /**
+     * 詳細
+     */
+    String description;
 }
--- src/main/java/com/example/demo/domain/model/Workspace.java
+++ src/main/java/com/example/demo/domain/model/Workspace.java
@@ -1,7 +1,13 @@
 package com.example.demo.domain.model;
 
+import java.util.List;
+
 /**
  * ワークスペース
  */
 public class Workspace {
+    /**
+     * タスク
+     */
+    List<Task> tasks;
 }
