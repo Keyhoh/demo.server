2d669aa700297bfb0d7bc2966d62908bfb5d9175
keyhoh <iheyhoh+github@gmail.com>
2021-05-03 18:54:42 +0900

    add: database

---
+++ README.md
@@ -0,0 +1,15 @@
+# DEMO SERVER
+
+## environment variables
+
+```shell
+export $(.env)
+```
+
+|name|value|
+|:---|:---|
+|DB|demo_db|
+|DB_USER|demo_user|
+|DB_PASSWORD|demo_password|
+|DB_PORT|51065|
+|DB_URL|jdbc:postgresql://localhost:${DB_PORT}/postgres|
--- src/test/java/com/example/demo/server/DemoApplicationTests.java
+++ src/test/java/com/example/demo/server/DemoApplicationTests.java
@@ -1,15 +1,13 @@
 package com.example.demo.server;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Disabled
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
